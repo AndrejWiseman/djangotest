a78358799fcb3b6b32a998e89a3c4b0ff5b0e344
Andrej <533737@gmail.com>
2024-05-09 16:03:07 +0200

    13

--- build_files.sh
+++ build_files.sh
@@ -9,6 +9,7 @@ python -m pip install --upgrade pip
 # Build the project
 echo "Building the project..."
 python -m pip install -r requirements.txt
+# pip install -r requirements.txt
 
 # Collect static files
 echo "Collecting static files..."
--- djangotest/settings.py
+++ djangotest/settings.py
@@ -43,8 +43,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 # STATIC_ROOT = BASE_DIR/'staticfiles'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 
 
 

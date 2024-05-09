efe36b19fd0f430ec3ff01f44782591348f6ded0
Andrej <533737@gmail.com>
2024-05-09 14:18:34 +0200

    4

--- build_files.sh
+++ build_files.sh
@@ -4,13 +4,13 @@ python3.12 -m venv venv
 source venv/bin/activate
 
 echo "Installing the latest version of pip..."
-python -m pip install --upgrade pip
+python3.12 -m pip install --upgrade pip
 
 # Build the project
 echo "Building the project..."
-python -m pip install -r requirements.txt
+python3.12 -m pip install -r requirements.txt
 
 # Collect static files
 echo "Collecting static files..."
 # python3.12 manage.py collectstatic --noinput
-python manage.py collectstatic --noinput --clear
+python3.12 manage.py collectstatic --noinput --clear

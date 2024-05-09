8cfb6d54e74a5dcd630e8319bb8c53030145a326
Andrej <533737@gmail.com>
2024-05-09 15:19:09 +0200

    11

--- djangotest/settings.py
+++ djangotest/settings.py
@@ -125,8 +125,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR/'static',]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 STATIC_ROOT = BASE_DIR/'staticfiles'
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static",

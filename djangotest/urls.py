ba331c1ca778ba41128189aea10e5528affb343d
Andrej <533737@gmail.com>
2024-05-09 18:13:20 +0200

    pwa

--- djangotest/urls.py
+++ djangotest/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('', include('pwa.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

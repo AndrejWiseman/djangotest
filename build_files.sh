5a115c422cb2d5f7a9f72eb6d094d92feae88ca6
Andrej <533737@gmail.com>
2024-05-09 16:05:49 +0200

    1

--- build_files.sh
+++ build_files.sh
@@ -9,7 +9,6 @@ python -m pip install --upgrade pip
 # Build the project
 echo "Building the project..."
 python -m pip install -r requirements.txt
-# pip install -r requirements.txt
 
 # Collect static files
 echo "Collecting static files..."
